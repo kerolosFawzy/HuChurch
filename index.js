bb544923fe68281a41c96e809a721cef5ae12031
kerolos fawzy <kerofawzy2055@gmail.com>
2023-07-15 15:30:07 +0300

    add "add form"

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
-
+app.use(express.static('public'));
 app.get('/', function (req, res) {
   res.render('home');
 });
 
 app.get('/add', function (req, res) {
   res.render('add');
+});
+
+app.post('/add', function (req, res) {
+  console.log(req.body);
 });
 app.listen(3000);
